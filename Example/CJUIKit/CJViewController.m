0547b195847272857f782a35494639439b99f1a0
fminor <563145721@qq.com>
2016-04-25 15:16:53 +0800

    加载更多

--- Example/CJUIKit/CJViewController.m
+++ Example/CJUIKit/CJViewController.m
@@ -19,11 +19,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self.navigationController.navigationBar setTranslucent:YES];
     
     _tableView = [[UITableView alloc] init];
     [_tableView setFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
     [_tableView setBackgroundColor:[UIColor whiteColor]];
-    [_tableView setSeparatorStyle:UITableViewCellSeparatorStyleSingleLine];
+    [_tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     [self.view addSubview:_tableView];
@@ -39,6 +40,22 @@
             [_sss->_tableView finishUpdate];
         });
     }];
+    
+    [_tableView setLoadMoreBlock:^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if ( _wss == nil ) {
+                return;
+            }
+            __strong CJViewController *_sss = _wss;
+            [_sss->_tableView finishUpdate];
+        });
+    }];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController.navigationBar setTranslucent:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -57,7 +74,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 3;
+    return 8;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
@@ -75,6 +92,18 @@
     UITableViewCell *_cell = [tableView dequeueReusableCellWithIdentifier:@"demo_cell"];
     if ( _cell == nil ) {
         _cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"demo_cell"];
+        
+        UIView *_sepLine = [[UIView alloc] init];
+        [_sepLine setTranslatesAutoresizingMaskIntoConstraints:NO];
+        [_sepLine setBackgroundColor:[UIColor blackColor]];
+        [_cell.contentView addSubview:_sepLine];
+        NSDictionary *_views = NSDictionaryOfVariableBindings(_sepLine);
+        [_cell.contentView addConstraints:[NSLayoutConstraint
+                                           constraintsWithVisualFormat:@"H:|[_sepLine]|"
+                                           options:0 metrics:nil views:_views]];
+        [_cell.contentView addConstraints:[NSLayoutConstraint
+                                           constraintsWithVisualFormat:@"V:[_sepLine(0.5)]|"
+                                           options:0 metrics:nil views:_views]];
     }
     return _cell;
 }
@@ -83,6 +112,7 @@
 {
     [cell.textLabel setText:[NSString stringWithFormat:@"%ld-%ld",
                              (long)indexPath.section + 1, (long)indexPath.row + 1]];
+    [cell.textLabel setBackgroundColor:[UIColor clearColor]];
 }
 
 @end
--- Pod/Classes/UITableView+CJUpdator.m
+++ Pod/Classes/UITableView+CJUpdator.m
@@ -66,6 +66,7 @@
         
         [self addGestureRecognizer:_endGesture];
         [self addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
+        [self addObserver:self forKeyPath:@"contentSize" options:NSKeyValueObservingOptionNew context:nil];
     } else {
     }
 }
@@ -112,10 +113,18 @@
 {
     if ( [self isRefreshStyle] ) {
         CJPullUpdatorView *_refreshView = [self _getPullUpdatorContainerForUpdatorStyle:CJUpdatorStyleRefresh];
-        [_refreshView stopAnimation];
+        CJPullUpdatorView *_loadmoreView = [self _getPullUpdatorContainerForUpdatorStyle:CJUpdatorStyleLoadmore];
         
         UIEdgeInsets _insets = self.contentInset;
-        _insets.top -= _refreshView.bounds.size.height;
+        if ( _refreshView && ( _refreshView.pullState == CJPullUpdatorViewStateAnimating )) {
+            [_refreshView stopAnimation];
+            _insets.top -= _refreshView.bounds.size.height;
+        }
+        if ( _loadmoreView && ( _loadmoreView.pullState == CJPullUpdatorViewStateAnimating )) {
+            [_loadmoreView stopAnimation];
+            _insets.bottom -= _loadmoreView.bounds.size.height;
+        }
+
         [UIView animateWithDuration:SCROLL_DURATION delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
             [self setContentInset:_insets];
         } completion:^(BOOL finished) {
@@ -170,7 +179,7 @@
     
     CJPullUpdatorView *_pullUpdatorContainer = [[CJPullUpdatorView alloc] init];
     CGRect _container_frame = CGRectMake(0,
-                                         _isRefresh ? (-DEFAULT_UPDATOR_HEIGHT) : self.bounds.size.height,
+                                         _isRefresh ? (-DEFAULT_UPDATOR_HEIGHT) : self.contentSize.height,
                                          self.bounds.size.width,
                                          DEFAULT_UPDATOR_HEIGHT);
     [_pullUpdatorContainer setFrame:_container_frame];
@@ -207,21 +216,57 @@
                         change:(NSDictionary<NSString *,id> *)change
                        context:(void *)context
 {
-    if ( ![keyPath isEqualToString:@"contentOffset"] ) {
+    if ( [keyPath isEqualToString:@"contentSize"] ) {
+        [self _actionContentSizeChanged];
+    }
+    
+    if ( [keyPath isEqualToString:@"contentOffset"] ) {
+        [self _actionContentOffsetChanged];
+    }
+}
+
+- (void)_actionContentSizeChanged
+{
+    CJPullUpdatorView *_loadmoreView = [self _loadmoreContainer];
+    if ( _loadmoreView == nil ) {
         return;
     }
     
-    CJPullUpdatorView *_refreshView = [self _refreshContainer];
-    if (( self.contentOffset.y <= - DEFAULT_UPDATOR_HEIGHT - self.contentInset.top )
-        && ( _refreshView.pullState == CJPullUpdatorViewStateNormal )) {
-        CJPullUpdatorView *_pullUpdatorView = [self _refreshContainer];
-        [_pullUpdatorView reverseImage];
+    if ( self.contentSize.height < self.bounds.size.height ) {
+        [self _removeLoadmoreContainer];
+        return;
     }
     
-    if (( self.contentOffset.y > - DEFAULT_UPDATOR_HEIGHT - self.contentInset.top )
-        && _refreshView.pullState == CJPullUpdatorViewStateReadyToRefresh ) {
-        CJPullUpdatorView *_pullUpdatorView = [self _refreshContainer];
-        [_pullUpdatorView resetImage];
+    [_loadmoreView setFrame:CGRectMake(0, self.contentSize.height,
+                                       self.bounds.size.width, DEFAULT_UPDATOR_HEIGHT)];
+}
+
+- (void)_actionContentOffsetChanged
+{
+    // 刷新
+    CJPullUpdatorView *_refreshView = [self _refreshContainer];
+    if ( _refreshView != nil ) {
+        if (( self.contentOffset.y <= - DEFAULT_UPDATOR_HEIGHT - self.contentInset.top )
+            && ( _refreshView.pullState == CJPullUpdatorViewStateNormal )) {
+            [_refreshView reverseImage];
+        }
+        
+        if (( self.contentOffset.y > - DEFAULT_UPDATOR_HEIGHT - self.contentInset.top )
+            && _refreshView.pullState == CJPullUpdatorViewStateReadyToRefresh ) {
+            [_refreshView resetImage];
+        }
+    }
+    
+    // 加载更多
+    CJPullUpdatorView *_loadmoreView = [self _loadmoreContainer];
+    if ( _loadmoreView != nil ) {
+        if (( self.contentOffset.y + self.bounds.size.height >= self.contentSize.height + DEFAULT_UPDATOR_HEIGHT + self.contentInset.bottom ) && _loadmoreView.pullState == CJPullUpdatorViewStateNormal ) {
+            [_loadmoreView reverseImage];
+        }
+        
+        if (( self.contentOffset.y + self.bounds.size.height < self.contentSize.height + DEFAULT_UPDATOR_HEIGHT + self.contentInset.bottom ) && _loadmoreView.pullState == CJPullUpdatorViewStateReadyToRefresh ) {
+            [_loadmoreView resetImage];
+        }
     }
 }
 
@@ -231,7 +276,7 @@
 {
     if ( [self isRefreshStyle] ) {
         CJPullUpdatorView *_refreshView = [self _refreshContainer];
-            if ( _refreshView.pullState == CJPullUpdatorViewStateReadyToRefresh ) {
+        if ( _refreshView.pullState == CJPullUpdatorViewStateReadyToRefresh ) {
             UIEdgeInsets _insets = self.contentInset;
             _insets.top += _refreshView.bounds.size.height;
             [UIView animateWithDuration:SCROLL_DURATION delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
@@ -240,9 +285,25 @@
             }];
             
             [_refreshView beginAnimation];
-            
             if ( _refreshView.updateAction ) {
                 _refreshView.updateAction();
+            }
+        }
+    }
+    
+    if ( [self isLoadmoreStyle] ) {
+        CJPullUpdatorView *_loadmoreView = [self _loadmoreContainer];
+        if ( _loadmoreView.pullState == CJPullUpdatorViewStateReadyToRefresh ) {
+            UIEdgeInsets _insets = self.contentInset;
+            _insets.bottom += _loadmoreView.bounds.size.height;
+            [UIView animateWithDuration:SCROLL_DURATION delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+                [self setContentInset:_insets];
+            } completion:^(BOOL finished) {
+            }];
+            
+            [_loadmoreView beginAnimation];
+            if ( _loadmoreView.updateAction ) {
+                _loadmoreView.updateAction();
             }
         }
     }
